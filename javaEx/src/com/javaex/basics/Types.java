673200c1d92986d208a3074bf6fc5b90409252f0
ShinJoongHo <az45687@naver.com>
2021-07-16 13:44:51 +0900

    논리형 데이터 타입

--- javaEx/src/com/javaex/basics/Types.java
+++ javaEx/src/com/javaex/basics/Types.java
@@ -5,7 +5,26 @@ public class Types {
 	public static void main(String[] args) {
 //		varEx();
 //		intergerEx();
-		floatDoubleEx();
+//		floatDoubleEx();
+		booleanEx();
+	}
+	
+	// 논리형 데이터 타입
+	private static void booleanEx() {
+		// 1byte: true or false
+		// 조건 분기, 반복문 등에 활용 -> 중요
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		
+		// 비교 연산, 논리 연산의 결과로 활용
+		int a = 7, b = 3;
+		boolean result = (a > b);
+		
+		System.out.println(result);
+		
 	}
 	// 실수형 데이터 타입
 	private static void floatDoubleEx() {
