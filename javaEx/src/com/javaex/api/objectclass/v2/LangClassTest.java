e61c161e7b63e3b08fe40a28c4e1f1b603ae0a97
pansu <az45687@naver.com>
2021-07-28 16:50:36 +0900

    test

--- javaEx/src/com/javaex/api/objectclass/v2/LangClassTest.java
+++ javaEx/src/com/javaex/api/objectclass/v2/LangClassTest.java
@@ -6,7 +6,7 @@ public class LangClassTest {
 		Point p = new Point(10, 20);
 		Point p2 = new Point(10, 20);
 		
-		System.out.println("p와 p2는 같은 객체?" + (p == p2));
+		System.out.println("p와 p2는 같은 객체? " + (p == p2));
 		// == 은 참조 주소 비교
 		// .equals() 는 내부 값의 비교
 		System.out.println("p와 p2는 같은 값을 가졌는가?" + p.equals(p2));
