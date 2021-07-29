fb14360df9fc0c9672cfeb3e3cf284d68c1b4945
pansu <az45687@naver.com>
2021-07-29 09:39:52 +0900

    my2

---
+++ javaEx/src/test/BookShop.java
@@ -0,0 +1,22 @@
+//package test;
+//
+//public class BookShop {
+//
+//	public static void main(String[] args) {
+//		System.out.println("*******도서정보 출력하기*****");
+//		displayBookInfo(books);
+//	
+//			for (Book book: books) {
+//				if (num == book.getBookNo()) {
+//					book.rent();
+//					break;
+//				}
+//			}
+//		}
+//	// (2) 전달받은 배열을 모두 출력하는 메소드
+//	private static void displayBookInfo(Book[] books) {
+//		for (Book book: books) {
+//			book.print();
+//		}
+//	}
+//}
---
+++ javaEx/src/testdefalut/MyInterface.java
@@ -0,0 +1,5 @@
+package testdefalut;
+// 기존 인터페이스
+public interface MyInterface {
+	public void method1();
+}
